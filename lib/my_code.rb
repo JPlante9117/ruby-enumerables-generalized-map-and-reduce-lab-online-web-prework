132deb45aa28e9ee5ad3d6ad75e6075462cedc26
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 13:39:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,11 @@
 # Your Code Here
+
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    yield (new.push( source_array[i] * -1 )) # <== Unique work
+    i += 1
+  end
+  return new
+end
