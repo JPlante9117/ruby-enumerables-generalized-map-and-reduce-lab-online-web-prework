a7cb633fdb77ad593e5205d42e1e9eff04b9c3b9
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 14:05:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def my own map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
+    yield new.push(source_array[i] * -1)
     i += 1
   end
   return new
