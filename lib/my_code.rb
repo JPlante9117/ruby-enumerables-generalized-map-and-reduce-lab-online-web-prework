76b48aaa3d4b2354a8ccf6edbce7cd49ff4d1b36
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 13:48:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,13 +1,3 @@
 # Your Code Here
 
-def my_own_map
-  new = []
-  i = 0
-  while i < source_array.length do
-    yield
-    i += 1
-  end
-  return new
-end
-
-my_own_map.map{|n| n * -1}
+map {|n| puts n * -1}
